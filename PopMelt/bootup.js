d7a03a4ba8b8ce037890f5370bde2b6ec4f2a7e2
Graham Reeves <graham@grahamreeves.com>
2020-04-19 13:31:10 +0100

    Initial ray march

--- PopMelt/bootup.js
+++ PopMelt/bootup.js
@@ -1,11 +1,28 @@
-
 Pop.Debug("Melttttttiiinnggg");
+
+Pop.Include = function(Filename)
+{
+	const Source = Pop.LoadFileAsString(Filename);
+	return Pop.CompileAndRun( Source, Filename );
+}
+
+Pop.Include('PopEngineCommon/PopShaderCache.js');
+Pop.Include('PopEngineCommon/PopFrameCounter.js');
+Pop.Include('PopEngineCommon/PopMath.js');
+Pop.Include('PopEngineCommon/ParamsWindow.js');
+Pop.Include('PopEngineCommon/PopCamera.js');
+
+Pop.Include('AssetManager.js');
+
 
 
 const Window = new Pop.Opengl.Window("Melt");
 Window.OnRender = GameRender;
 Window.OnMouseMove = function(){};
 
+const Params = {};
+Params.ClearColour = [0.8,0.5,0.1];
+Params.FovVertical = 45;
 
 function GameRender(RenderTarget)
 {
@@ -19,12 +36,49 @@ let RenderGameFunc = null;
 
 function MeltGameRender(RenderTarget,GameState)
 {
-	RenderTarget.ClearColour(0.8,0.5,0.1);
+	const State = GameState.State;
+	const Runtime = GameState.Runtime;
+	RenderTarget.ClearColour(...Params.ClearColour);
+	
+	Runtime.Camera.FovVertical = Params.FovVertical;
+	
+	const Quad = GetAsset('Quad',RenderTarget);
+	const Shader = GetAsset(Runtime.SceneShader,RenderTarget);
+	const Camera = Runtime.Camera;
+	
+	const WorldToCameraMatrix = Camera.GetWorldToCameraMatrix();
+	const CameraProjectionMatrix = Camera.GetProjectionMatrix( RenderTarget.GetScreenRect() );
+	const ScreenToCameraTransform = Math.MatrixInverse4x4( CameraProjectionMatrix );
+	const CameraToWorldTransform = Math.MatrixInverse4x4( WorldToCameraMatrix );
+	const LocalToWorldTransform = Camera.GetLocalToWorldFrustumTransformMatrix();
+	//const LocalToWorldTransform = Math.CreateIdentityMatrix();
+	const WorldToLocalTransform = Math.MatrixInverse4x4(LocalToWorldTransform);
+
+	const SetUniforms = function(Shader)
+	{
+		function SetUniform(Key)
+		{
+			Shader.SetUniform( Key, Params[Key] );
+		}
+		Object.keys(Params).forEach(SetUniform);
+		
+		Shader.SetUniform('VertexRect',[0,0,1,1]);
+		Shader.SetUniform('ScreenToCameraTransform',ScreenToCameraTransform);
+		Shader.SetUniform('CameraToWorldTransform',CameraToWorldTransform);
+		Shader.SetUniform('LocalToWorldTransform',LocalToWorldTransform);
+		Shader.SetUniform('WorldToLocalTransform',WorldToLocalTransform);
+	}
+	RenderTarget.SetBlendModeAlpha();
+	RenderTarget.DrawGeometry( Quad, Shader, SetUniforms );
 }
 
 async function LoadAssets()
 {
-	const AssetFilenames = [];
+	const AssetFilenames =
+	[
+	'SceneMarch.frag.glsl',
+	'Quad.vert.glsl'
+	];
 	const AssetPromises = AssetFilenames.map( Pop.LoadFileAsStringAsync );
 	await Promise.all(AssetPromises);
 	return true;
@@ -38,7 +92,13 @@ async function ResetGame()
 	
 	//	game state
 	const Game = {};
-	Game.Render = function(RenderTarget)	{	MeltGameRender(RenderTarget,Game);	}
+	Game.State = {};
+	
+	Game.Runtime = {};
+	Game.Runtime.Render = function(RenderTarget)	{	MeltGameRender(RenderTarget,Game);	}
+	Game.Runtime.Camera = new Pop.Camera();
+	Game.Runtime.SceneShader = RegisterShaderAssetFilename('SceneMarch.frag.glsl','Quad.vert.glsl');
+
 	return Game;
 }
 
@@ -55,7 +115,7 @@ async function WaitForUserRestart()
 
 function GameIteration(GameState,Time,FrameDuration)
 {
-	Pop.Debug(`Game Iteration ${Time}`);
+	//Pop.Debug(`Game Iteration ${Time}`);
 }
 
 async function GameLoop(GameState)
@@ -91,9 +151,9 @@ async function AppLoop()
 	while ( true )
 	{
 		const GameState = await ResetGame();
-		RenderGameFunc = GameState.Render;
+		RenderGameFunc = GameState.Runtime.Render;
 		await WaitForUserStart();
-		const GameResult = await GameLoop(GameState);
+		const GameResult = await GameLoop(GameState.State);
 		ShowGameResult(GameResult);
 		await WaitForUserRestart();
 	}
