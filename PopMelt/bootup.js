dc4106120abb6a2821a2ea4904baf493487d61f4
Graham Reeves <graham@grahamreeves.com>
2020-04-20 14:48:36 +0100

    env box on by default

--- PopMelt/bootup.js
+++ PopMelt/bootup.js
@@ -82,7 +82,7 @@ Params.ShowDepth = false;
 Params.ShowDepthFar = 30;
 Params.PassJitter = 0.001;
 Params.FloorTileSize = 10;
-Params.RenderEnvironmentSkybox = false;
+Params.RenderEnvironmentSkybox = true;
 
 const ParamsWindow = new Pop.ParamsWindow(Params,function(){});
 ParamsWindow.AddParam('RefractionScalar',0,1);
